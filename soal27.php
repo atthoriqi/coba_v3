eb2412cbf575ea36049af2771200bc6a9c723d7f
atthoriqi <thoriq1997@gmail.com>
2016-07-22 09:17:35 +0700

    coba

---
+++ soal27.php
@@ -0,0 +1,29 @@
+<?php
+for ($a=1; $a<=16; $a+1){
+$nilai=$nilai_POST['nilai'];
+if($nilai==16){
+	echo "pondok".$a;
+}
+elseif($nilai==5){
+	echo "programmer";
+}
+elseif($nilai==6){
+	echo "pondok";
+}
+elseif($nilai==9){
+	echo "pondok";
+}
+elseif($nilai==10){
+	echo "programmer";
+}
+elseif($nilai==12){
+	echo "pondok";
+}
+elseif($nilai==15){
+	echo "pondok";
+}
+elseif($nilai==16){
+ echo "programmer";
+}
+
+ ?>
